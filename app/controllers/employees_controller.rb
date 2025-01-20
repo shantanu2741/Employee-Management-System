ded874179b69686f45f7be7c9a27b3d8d7f0dee6
shantanuk14-admin <shantanukhokrale14@gmail.com>
2025-01-21 01:09:04 +0530

    Added employee view functionality

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,49 +1,79 @@
 class EmployeesController < ApplicationController
-    def index
-      @employees = Employee.all
-    end
-  
-    def new
-      @employee = Employee.new
-    end
-  
-    # private
-    def employee_params
-      params.require(:employee).permit(:first_name, :last_name, :city, :state, :country, :email)
-    end
-  
-  
-  
-    def create
-      @employee = Employee.new(employee_params)
-  
-      if @employee.save
-        redirect_to employees_path, notice: "Employee has been created successfully"
-      else
-        render :new
-      end
-    end
-    # def edit
-    #   @employee = Employee.find_by(id: params[:id])
-    # end
-    def update
-        @employee = Employee.find(params[:id])
-        if @employee.update(employee_params)
-          redirect_to employees_path, notice: "Employee updated successfully."
-        else
-          render :edit
-        end
-      end
-
-      def delete
-        @employee = Employee.find(params[:id])
-        if @employee.destroy(employee_params)
-          redirect_to employees_path, notice: "Employee deleted successfully."
-        end
-      end
-      
+  def index
+    @employees = Employee.all
   end
 
+  def new
+    @employee = Employee.new
+  end
 
-#   <%# <%= form_with model: @employee do |form| %> 
-# <%# <%= form_with model: @employee, local: true do |form| %> %>
+  # private
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :city, :state, :country, :email)
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+
+    if @employee.save
+      redirect_to employees_path, notice: "Employee has been created successfully"
+    else
+      render :new
+    end
+  end
+  def edit
+    @employee = Employee.find_by(id: params[:id])
+  end
+  def update
+    @employee = Employee.find_by(id: params[:id])
+    if @employee.update(employee_params)
+      redirect_to employees_path, notice: "Employee has been Updated successfully"
+    else
+      render :edit
+    end
+  end
+
+  # def destroy
+  #   debugger
+  #   @employee = Employee.find_by(id: params[:id])
+  #   if @employee.destroy(employee_params)
+  #     redirect_to employees_path, notice: "Employee has been deleted successfully"
+  #   end
+  # end
+
+  
+  # def destroy
+  #   @employee = Employee.find_by(id: params[:id])
+  #   if @employee&.destroy
+  #     redirect_to employees_path, notice: "Employee has been deleted successfully"
+  #   else
+  #     redirect_to employees_path, alert: "Employee could not be deleted"
+  #   end
+  # end
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+    redirect_to employees_path, notice: "Employee was successfully deleted."
+  end
+  def show
+    @employee = Employee.find_by(id: params[:id])
+  end
+end
+
+# def edit
+#   @employee = Employee.find_by(id: params[:id])
+
+#   if @employee.nil?
+#     redirect_to employees_path, alert: "Employee not found."
+#   elsif @employee.update(employee_params)
+#     redirect_to employees_path, notice: "Employee has been updated successfully."
+#   else
+#     render :edit
+#   end
+# end
+
+# if @employee.nil?
+#   redirect_to employees_path, alert: "Employee not found."
+# else
+#   @employee = Employee.update()
+# end
